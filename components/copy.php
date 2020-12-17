72124cbc30816189bf0bae65ea5d2538195ec297
jumb012 <54991942+jumb012@users.noreply.github.com>
2020-12-16 18:35:04 -0700

    Update copy.php

--- components/copy.php
+++ components/copy.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
   <div class="container-fluid bg-light py-2">
-    <p class="text-center text-1 text-muted mb-0">Copyright © 2020 <a href="/ht">PayPal</a>. All Rights Reserved.</p>
+    <p class="text-center text-1 text-muted mb-0">Copyright © 2020 <a href="https://wwww.clonepay.tk">PayPal</a>. All Rights Reserved.</p>
   </div>
 </html>
